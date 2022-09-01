b0677fe13741b5e7c46389c2b6164a123e202e1d
NastyaM <Nastya@mail.com>
2022-09-01 22:15:22 +0700

    DZ#52

---
+++ DZ_52/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] GetArray (int m, int n)
+{
+int [,] matrix = new int[m,n];
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+            {
+            matrix[i,j] = new Random().Next(11);
+            }
+    }
+return matrix;
+}
+
+int [,] result = GetArray(rows,columns); 
+
+ void PrintArray (int [,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++) 
+            {
+            Console.Write(inputMatrix[i,m] + "\t");
+            }
+    Console.WriteLine();
+    }
+}
+
+PrintArray(result);
